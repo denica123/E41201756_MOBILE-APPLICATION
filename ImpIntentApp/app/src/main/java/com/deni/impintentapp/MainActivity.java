ff8d353c24dcf3a2e92bc2fb9f6e1af1aa836338
Deni Catur Kusumawati <75110391+denica123@users.noreply.github.com>
2021-10-14 00:42:32 +0700

    Tugas ImpIntent

---
+++ ImpIntentApp/app/src/main/java/com/deni/impintentapp/MainActivity.java
@@ -0,0 +1,62 @@
+package com.deni.impintentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    EditText editText;
+
+    EditText name;
+    Button btnSend;
+    private String KEY_NAME = "NAMA";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //INTENT IMPLICIT
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = editText.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+        //INTENT EXPLICIT
+        name = (EditText) findViewById(R.id.edt_nama);
+        btnSend = (Button) findViewById(R.id.btn_send);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String nama = name.getText().toString();
+                    if (nama != ""){
+                        Intent i = new Intent(MainActivity.this, second.class);
+                        i.putExtra(KEY_NAME, nama);
+                        startActivity(i);
+                    } else {
+                        Toast.makeText(getApplication(), "YOU NEED TO FILL YOUR NAME",Toast.LENGTH_SHORT);
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                    Toast.makeText(getApplication(), "ERROR, TRY AGAIN !",Toast.LENGTH_SHORT);
+                }
+            }
+        });
+
+    }
+}
